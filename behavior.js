c42a79180d7ac5a125d3b28f1aec807b79037099
Aryeh Mischel <aryehmischel@gmail.com>
2023-07-19 09:28:16 -0400

    changed to correct branch

--- behavior.js
+++ behavior.js
@@ -1,166 +1,13 @@
-AFRAME.registerComponent("showlist", {
+//Εμφάνιση db στο console, ασύγχρονα. Δεν λειτουργεί!!
 
-	init: function(){
-		let list = document.querySelector('#list');
-
-
-		 this.el.addEventListener('click',function(obj){ //obj == To clickable object
-			let p = obj.srcElement.object3D.position; // p I thesi tou antikeimenou pou egine clicked
-			list.setAttribute('visible','true'); // Emfanisi listas
-		 })
-	},
-});
-
-AFRAME.registerComponent("hidelist", {
-
-	init: function(){
-		let list = document.querySelector('#list');
-
-		this.el.addEventListener('click',function(obj) {
-
-			list.setAttribute('position','0 200 0'); // Allazw position giati me to "visible=flase" sinexizei na allilepidra o cursor 
-			console.log('asdasd');
-		})
-
-	},
-
-});
-
-
-AFRAME.registerComponent("south", {
-
-	init: function(){
-		let list = document.querySelector('#list');
-
-		this.el.addEventListener('click',function(obj) {
-			let p = obj.srcElement.object3D.position;
-
-			list.setAttribute('rotation','0 180 0');
-
-			let newpX = p.x -2.5;
-			let newpY = p.y + 1;
-			let newpz = p.z;
-			list.setAttribute('position',newpX + " " + newpY + " "+ p.z );
-		})
-
-	},
-
-});
-
-AFRAME.registerComponent("north", {
-
-	init: function(){
-		let list = document.querySelector('#list');
-
-		this.el.addEventListener('click',function(obj) {
-			let p = obj.srcElement.object3D.position;
-
-			list.setAttribute('rotation','0 0 0');
-
-			let newpX = p.x -2.5;
-			let newpY = p.y + 1;
-			let newpz = p.z;
-			list.setAttribute('position',newpX + " " + newpY + " "+ p.z );
-		})
-
-	},
-
-});
-
-AFRAME.registerComponent("east", {
-
-	init: function(){
-		let list = document.querySelector('#list');
-
-		this.el.addEventListener('click',function(obj) {
-			let p = obj.srcElement.object3D.position;
-
-			list.setAttribute('rotation','0 -90 0');
-
-			let newpX = p.x -1;
-			let newpY = p.y + 1;
-			let newpz = p.z;
-			list.setAttribute('position',newpX + " " + newpY + " "+ p.z );
-		})
-
-	},
-
-});
-
-AFRAME.registerComponent("west", {
-
-	init: function(){
-		let list = document.querySelector('#list');
-
-		this.el.addEventListener('click',function(obj) {
-			let p = obj.srcElement.object3D.position;
-
-			list.setAttribute('rotation','0 90 0');
-
-			let newpX = p.x +2.5;
-			let newpY = p.y + 1;
-			let newpz = p.z;
-			list.setAttribute('position',newpX + " " + newpY + " "+ p.z );
-		})
-
-	},
-
-});
-
-AFRAME.registerComponent("test", {
-
-	init: function(){
-	let base = document.querySelector('#modelbase');
-	let p = base.getAttribute('position');
-	let scene = document.querySelector('a-scene');
-
-
-	this.el.addEventListener('mouseenter',function(obj){
-		
-		//let id = obj.path[0].id;
-		 let id =obj.srcElement.id;
-
-		if(id=='cube1'){
-			//console.log(p);
-			let cube = document.createElement('a-entity');
-			cube.setAttribute('geometry',{
-				primitive: 'box'
-			});
-			cube.setAttribute('material', 'color:red');
-			cube.setAttribute('scale', '0.5 0.5 0.5');	
-			cube.setAttribute('position','0 0.6 0');
-			//cube.setAttribute('preview');  Thelw na dosei component gia na kanei rotation
-			base.appendChild(cube);
-			//console.log(cube);
-		}
-
-
-			if(id=='cube2'){
-			//console.log(p);
-			let cube = document.createElement('a-entity');
-			cube.setAttribute('geometry',{
-				primitive: 'box'
-			});
-			cube.setAttribute('material', 'color:green');
-			cube.setAttribute('scale', '0.5 0.5 0.5');			
-			cube.setAttribute('position','0 0.6 0');
-			base.appendChild(cube);
-			//console.log(cube);
-		}
-
-
-		});
-
-
-	},
-
-
-});
-
+// fetch('test.php')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 
 
 //import data from '/WebCatalog/content.json' assert { type: 'JSON' };
 //const exhibits = require("./WebCatalog/content.json");
+
 
 let data;
 let response;
@@ -170,6 +17,8 @@ async function fetchContent()
 	//response = await fetch("http://localhost:8000/WebCatalog/content.json"); // works - needs data = await response.json(); 
 	response = await fetch("details.json"); // works - needs data = await response.json(); 
 	data = await response.json();
+
+
 	
 	//const response = await fetch("http://localhost:8000/WebCatalog/content.json").then( response =>{console.log(response);}); // works and gets response
 	//const response = await fetch("http://localhost:8000/WebCatalog/content.json").then(response => {data = response.json(); loadExhibit();});
@@ -180,8 +29,6 @@ function loadExhibit()
 {
 	//console.log(response);
 	//console.log(data);
-	
-	
 	if (response == null)
 	{
 		console.log("Not ready yet!");
@@ -192,39 +39,291 @@ function loadExhibit()
 		console.log("Data loaded! ") 
 		//console.log(data);
 	}
-	
-	
-	
-	// let fields = document.getElementsByClassName("vcorfu-content");
-	
-	// for (let i = 0; i < fields.length; i++)
-	// {
-	// 	console.log(fields[i].getAttribute('data-field'));
+}
+
+	let base;
+	let previousBase;
+	let sameBase;
+	let count=4;
+	let text="";
+
+
+	AFRAME.registerComponent("show-list", {
+
+	init: function(){
+	var myDiv = document.querySelector("#myDiv");
+		 this.el.addEventListener('click',function(){
+		 		base=this; // Αποθήκευση βάσης που επιλέχθηκε από τον χρήστη
+		 		
+				//<!-- ...............  -->
+
+				// Εδώ θα τρέχει μια function για να τσεκάρει αν υπάρχει αυτή η βάση στην βάση δεδομένων. Αν όχι, τότε θα κάνει INSERT!
+
+				//<!-- ...............  -->
+				// myDiv.style.visibility = "visible"
+				myDiv.innerHTML = displayDescription();
+
+				checkBase();
+			 	console.log(base.id); 
+			 	// if(sameBase==true){
+			 	// 	if(myDiv.style.visibility==="hidden"){
+			 	// 		myDiv.style.visibility = "visible";
+			 	// 	}
+			 	// 	else{
+			 	// 		myDiv.style.visibility= "hidden";
+			 	// 	}
+				//  }
+			 	// if(sameBase==false){
+			 	// 	myDiv.style.visibility = "visible";
+			 	// }
+		 })
+	},
+});
+
+	function displayDescription(){
+		if(text==="")
+		{		
+				text+= "<ul>";
+				text+= "<h4><i>Διάλεξε ποιο έκθεμα θέλεις να τοποθετηθεί στην βάση</i>: (" + base.id + ")</h4>";
+				for(var i=0; i<data.exhibits.length; i++){			
+					text+= "<li id="+data.exhibits[i].id+" onclick='placeExhibit(this)'> <b>"+data.exhibits[i].title+ "</b>" + ": "; //Με το "this" παίρνω τα στοιχεία του κειμένου που επιλέχθηκε από τον χρήστη, συνεπώς και το έκθεμα που επέλεξε.
+					text+= data.exhibits[i].description+ " <br></li>";
+				}
+				text+= "</ul>";
+				//console.log(text);
+				return text;
+			}
+			else{
+				text="";
+				return text;
+				// console.log(myDiv.innerHTML);
+			}
+	}
+
+	function checkBase(){
+		if (base==previousBase){		
+			sameBase=true;
+			//console.log("einai idies");
+		}	
+		if(base!=previousBase){	
+			sameBase = false;
+			//console.log("DEN einai idies");
+		}
+		previousBase = base;
+	}
+
+function placeExhibit(entity){
+		//console.log(exhibit);
+
+		var id = entity.getAttribute('id');
+		// console.log(id);		
+		console.log(this.exhibit);
+			if(this.exhibit){
+					var exhibitId = this.exhibit.getAttribute('id');
+					console.log(exhibitId);
+
+				if(this.exhibit.parentNode==base){
+					// console.log(this.exhibit.parentNode);
+
+					if(exhibitId!= id + "." + id){
+					removeChild(base);
+
+	    			var exhibit = document.createElement('a-entity');
 		
-	// 	switch (fields[i].getAttribute('data-field'))
-	// 	{
-	// 		case "title":
-	// 			fields[i].innerHTML = data.exhibits[0].title[parameters.language];
-	// 		break;
-			
-	// 		case "text":
-	// 			fields[i].innerHTML = data.exhibits[0].text[parameters.language];
-	// 		break;
-			
-	// 		case "3d-model":
-			
-	// 		break;
-			
-	// 		case "images":
-			
-	// 		break;
-			
-	// 		case "map-field":
-	// 			//fields[i].innerHTML += data.exhibits[0].gpsCoordinates.lat + ", " + data.exhibits[0].gpsCoordinates.long;
-	// 			fields[i].innerHTML = "";
-	// 			showExhibitOnTheMap(data.exhibits[0].gpsCoordinates.lat, data.exhibits[0].gpsCoordinates.long);
-	// 		break;
-	// 	}
-	// }
+						exhibit.setAttribute('position',0 +" " + 1.7 +" " + 0); 
+						// exhibit.setAttribute('position', { x: base.object3D.position.x, y: base.object3D.position.y + 1, z: base.object3D.position.z });
+						if(id!=0)
+						exhibit.setAttribute('gltf-model',`url(${data.exhibits[id].pathfile})`);
+						exhibit.setAttribute('scale',data.exhibits[id].scale); // αλλαγή του scale διότι το 2ο έκθεμα ήταν τεράστιο.
+						exhibit.setAttribute('id',id+"."+id);
+
+						base.appendChild(exhibit);
+						this.exhibit = exhibit;
+						storeData();
+					}
+
+				}
+				if(this.exhibit.parentNode!=base){
+
+					if(exhibitId!=id + "." + id){
+						removeChild(base);						
+						var exhibit = document.createElement('a-entity');
+						exhibit.setAttribute('position',0 +" " + 1.7 +" " + 0); 
+						// exhibit.setAttribute('position', { x: base.object3D.position.x, y: base.object3D.position.y + 1, z: base.object3D.position.z });
+						if(id!=0)
+						exhibit.setAttribute('gltf-model',`url(${data.exhibits[id].pathfile})`);
+						exhibit.setAttribute('scale',data.exhibits[id].scale); // αλλαγή του scale διότι το 2ο έκθεμα ήταν τεράστιο.
+						exhibit.setAttribute('id',id+"."+id);
+						base.appendChild(exhibit);
+						this.exhibit = exhibit;
+						storeData();
+
+					}
+					else{
+						// this.exhibit.parentNode.removeChild(this.exhibit);
+	    				// this.exhibit = null;
+						removeChild(base);
+
+	    				var exhibit = document.createElement('a-entity');
+		
+						exhibit.setAttribute('position',0 +" " + 1.7 +" " + 0); 
+						// exhibit.setAttribute('position', { x: base.object3D.position.x, y: base.object3D.position.y + 1, z: base.object3D.position.z });
+						if(id!=0)
+						exhibit.setAttribute('gltf-model',`url(${data.exhibits[id].pathfile})`);
+						exhibit.setAttribute('scale',data.exhibits[id].scale); // αλλαγή του scale διότι το 2ο έκθεμα ήταν τεράστιο.
+						exhibit.setAttribute('id',id+"."+id);
+
+						base.appendChild(exhibit);
+						this.exhibit = exhibit;
+						storeData();
+					}
+				}						
+			}
+			else{
+					removeChild(base);				
+
+					var exhibit = document.createElement('a-entity');
+		
+					exhibit.setAttribute('position',0 +" " + 1.7 +" " + 0); 
+					// exhibit.setAttribute('position', { x: base.object3D.position.x, y: base.object3D.position.y + 1, z: base.object3D.position.z });
+					if(id!=0)
+					exhibit.setAttribute('gltf-model',`url(${data.exhibits[id].pathfile})`);
+					exhibit.setAttribute('scale',data.exhibits[id].scale); // αλλαγή του scale διότι το 2ο έκθεμα ήταν τεράστιο.
+					exhibit.setAttribute('id',id+"."+id);
+					base.appendChild(exhibit);
+					console.log(exhibit);
+					this.exhibit = exhibit;
+					storeData();
+			}
+	function storeData(){
+	  $.ajax({
+	  url: "sqlite.php",
+	  method: "POST",
+	  data: { id:base.id, exhibit:data.exhibits[id].id , action:"store"},
+	  success: function(response) {
+	    console.log("Selection stored successfully.");
+	   	//console.log(response);
+	  },
+	  		error: function(xhr, status, error) {
+	    	console.log("An error occurred: " + error);
+	  		}
+		});
+	}
+}
+
+
+	function retrieveData(){
+		$.ajax({
+			url:"sqlite.php",
+			method:"POST",
+			data: {action:"view"},
+			success: function(json) {
+	    		console.log("Success Response");
+	    		var json = JSON.parse(json);
+	    		//console.log(json);
+				if (data == null)
+				{
+					console.log("2nd Not ready yet!");
+					setTimeout(retrieveData(),1);
+				}
+				else{
+		    		for (var i=0; i<json.length; i++){
+		    		count = json.length;
+
+					var stand = document.createElement('a-entity');
+					stand.setAttribute('id',data.stands[i].id);
+					stand.setAttribute('show-list','show-list');
+					stand.setAttribute('position',data.stands[i].position);
+					stand.setAttribute('gltf-model',`url(${data.stands[i].pathfile})`);
+					stand.setAttribute('rotation',data.stands[i].rotation);
+					// stand.setAttribute('scale',data.stands[i].scale);
+					stand.setAttribute('class','clickable stand');
+					scene.appendChild(stand);
+
+		    			// base = document.getElementById(i); THA XRISIMOPOIISW TIN METAVLITI "STAND"
+		    			removeChild();
+						//console.log(json);
+						if(json[i]!=null){
+		    				var exhibit = document.createElement('a-entity');
+							exhibit.setAttribute('position',0 +" " + 1.7 +" " + 0);
+							if(json[i]!=0)
+							exhibit.setAttribute('gltf-model',`url(${data.exhibits[json[i]].pathfile})`);
+							exhibit.setAttribute('scale',data.exhibits[json[i]].scale);
+							exhibit.setAttribute('id',json[i]+"."+json[i]);
+							exhibit.setAttribute('class','clickable');
+							stand.appendChild(exhibit);
+						}
+					}					
+	    		}
+	  		},
+		
+		});
+	}
+
+function removeChild(){
+		if(base!=null){
+		if(base.childNodes){
+			for (var k = base.childNodes.length -1; k >= 0; k--) {
+   				if (base.childNodes[k].tagName === 'A-ENTITY') {
+      				base.removeChild(base.childNodes[k]);
+    			}	
+			}
+		}
+	}
+}
+
+function addBases(){
+	if(data.stands.length>count){
+		var stand = document.createElement('a-entity');
+		stand.setAttribute('id',data.stands[count].id);
+		stand.setAttribute('show-list','show-list');
+		stand.setAttribute('position',data.stands[count].position);
+		stand.setAttribute('gltf-model',`url(${data.stands[count].pathfile})`);
+		stand.setAttribute('rotation',data.stands[count].rotation);
+		stand.setAttribute('class','clickable stand');
+		scene.appendChild(stand);
+		count++;
+		base = stand;
+		addBaseToServer();
+		//console.log(base);
+	}
+	else
+		return;
+}
+
+function removeBases(){
+	if(count>4){
+		removeBaseFromServer();
+		
+		var stand = document.getElementById(count);
+		console.log(stand);
+		scene.removeChild(stand);
+		count--;
+	}
+
 	
 }
+
+
+	function addBaseToServer(){
+		console.log('Prosthese vasi');
+		$.ajax({
+			url: "sqlite.php",
+			method:"POST",
+			data: {action:"add"},
+			success: function(response) {
+		    console.log("Base added successfully.");
+		  	}
+		});
+	}
+
+	function removeBaseFromServer(){
+		$.ajax({
+			url: "sqlite.php",
+			method:"POST",
+			data: {action:"remove"},
+			success: function(response) {
+		    console.log("Base removed successfully.");
+		  	}
+		});
+	}	
--- test.php
+++ test.php
@@ -2,130 +2,116 @@
 <html>
 <head>
 	<script src="https://aframe.io/releases/1.4.0/aframe.min.js"></script>
-	<title>test json</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <!-- <script src="https://raw.githack.com/AR-js-org/AR.js/3.4.5/aframe/build/aframe-ar-nft.js"></script> -->
+  <!-- <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script> -->
+
+	<title>Test 0.1V</title>
 	<script src="behavior.js"></script>
+	<?php require "sqlite.php" ?>
+
 </head>
 
 <style>
 	#myDiv{
-  position: relative;
+
+  position: absolute;
 /*  background-color: black;*/
-  top:100px;
+  top:50px;
   z-index: 5;
   color: black;
 /*  text-align: center;*/
+	}
+	ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+	}
+	h4{
+	display:table;
+	padding: 5px;
+  margin: 5px 0;
+  background-color: #FA6B4F;
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, .7);
+	}
+	li{
+	display:list-item;
+	padding: 2px;
+  margin: 5px 0;
+  background-color: #F3C5BC60;
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, .7);
 	}
 </style>
 
 <script>
 	fetchContent(); // LOAD JSON FILE !!
-
-	let base;
-	let previousBase;
-	let sameBase;
-
-	AFRAME.registerComponent("show-list", {
-
-	init: function(){
-	var myDiv = document.querySelector("#myDiv");
-
-
-		 this.el.addEventListener('click',function(){
-		 		base=this; // Αποθήκευση βάσης που επιλέχθηκε από τον χρήστη
-				myDiv.innerHTML = displayDescription();
-
-				checkBase();
-			 	console.log(base.id); 
-			 	if(sameBase==true){
-			 		if(myDiv.style.visibility==="hidden"){
-			 			myDiv.style.visibility = "visible";
-			 		}
-			 		else{
-			 			myDiv.style.visibility= "hidden";
-			 		}
-				 }
-			 	if(sameBase==false){
-			 		myDiv.style.visibility = "visible";
-			 	}
-
-		 })
-	},
-});
-
-	function checkBase(){
-		if (base==previousBase){		
-			sameBase=true;
-			//console.log("einai idies");
-		}	
-		if(base!=previousBase){	
-			sameBase = false;
-			//console.log("DEN einai idies");
-		}
-		previousBase = base;
-	}
-
-
-	function displayDescription(){
-		var text="";
-		text+= "<ul>";
-		text+= "<h4><i>Διάλεξε ποιο έκθεμα θέλεις να τοποθετηθεί στην βάση</i>: " + base.id + "</h4>";
-		for(var i=0; i<data.exhibits.length; i++){			
-			text+= "<li> <i id="+data.exhibits[i].id+" onclick='placeExhibit(this)'> "+data.exhibits[i].title+ "</i>" + ": "; //Με το "this" παίρνω τα στοιχεία του κειμένου που επιλέχθηκε από τον χρήστη, συνεπώς και το έκθεμα που επέλεξε.
-			text+= data.exhibits[i].description+ " <br></li>";
-		}
-		text+= "</ul>";
-		//console.log(text);
-		return text;
-	}
-
-
-function placeExhibit(entity){
-
-		var id = entity.getAttribute('id');
-		
-		var exhibit = document.createElement('a-entity');
-		//console.log(id);
-		exhibit.setAttribute('position',base.object3D.position.x +" " + base.object3D.position.y+1 +" " + base.object3D.position.z); 
-		// exhibit.setAttribute('position', { x: base.object3D.position.x, y: base.object3D.position.y + 1, z: base.object3D.position.z });
-		exhibit.setAttribute('gltf-model',`url(${data.exhibits[id].pathfile})`);
-		exhibit.setAttribute('scale',data.exhibits[id].scale); // αλλαγή του scale διότι το 2ο έκθεμα ήταν τεράστιο.
-		scene.appendChild(exhibit);
-}	
+	retrieveData();
+	console.log();
 
 </script>
 
 <body onload="loadExhibit()" ></body>
 
 	
-		  <div id="myDiv" style="visibility: hidden;" >
-
-		  </div> 
+	<div id="myDiv"></div> <!--ΑΝ ΜΕΤΑΚΙΝΗΣΩ ΤΟ DIV ΔΕΝ ΘΑ ΛΕΙΤΟΥΡΓΕΙ ΣΩΣΤΑ Η ΕΜΦΑΝΙΣΗ ΤΗΣ ΛΙΣΤΑΣ -->
 	
-<a-scene id="scene">
+
+ <a-scene id="scene">
 
 				<a-assets>
 
 					<a-asset-items id="building" src="Building/building.gltf"></a-asset-items>
+					<a-asset-itme id="stand" src="museum_case/scene.gltf"></a-asset-itme>
 
 				</a-assets>
 
 
 <a-sky color="lightblue"></a-sky>
 
+<a-box class="clickable" onclick="addBases()" color="green" position="1 0 -2"></a-box>
+
+<a-box class="clickable" onclick="removeBases()" color="red" position="-1 0 -2"></a-box>
 
 
-		 <a-box id="Κόκκινη βάση" show-list class="clickable" color="red" position="-3 0 -5"></a-box> <!--Έχω βάλει ελληνικά id και με κενό!!! -->
 
-		 <a-box id="Πράσινη βάση" show-list class="clickable" color="green" position="1 0 -5"></a-box> <!-- Έχω βάλει ελληνικά id και με κενό!!! -->
+<!-- 	<a-entity id="1" show-list gltf-model="#stand" class="clickable" position="-16 -.5 -2.5" rotation="0 -90 0"></a-entity>
 
-			<a-camera id="camera">
+	<a-entity id="2" show-list gltf-model="#stand" class="clickable" position="-2 -.5 -7" rotation="0 -90 0"></a-entity>
+
+	<a-entity id="3" show-list gltf-model="#stand" class="clickable" position="1.2 -.5 -7" rotation="0 90 0"></a-entity>
+
+	<a-entity id="4" show-list gltf-model="#stand" class="clickable" position="14.5 -.5 -2.5" rotation="0 90 0"></a-entity> -->
+
+<!-- 	<a-entity id="5" show-list gltf-model="#stand" class="clickable" position="-2 -.5 -10" rotation="0 -90 0"></a-entity>
+
+	<a-entity id="6" show-list gltf-model="#stand" class="clickable" position="1.2 -.5 -10" rotation="0 90 0"></a-entity> -->
+
+
+
+		<!-- <a-box id="1" show-list class="clickable" color="yellow" position="-16 0 -2.5" rotation="0 -90 0"></a-box> 
+
+		<a-box id="2" show-list onclick="myFunction()" class="clickable" color="red" position="-2 0 -7" rotation="0 -90 0"></a-box> Έχω βάλει ελληνικά id και με κενό!!! 
+
+		 <a-box id="3" show-list class="clickable" color="green" position="1 0 -7" rotation="0 90 0"></a-box> 
+		 
+		 <a-box id="4" show-list class="clickable" color="blue" position="14.5 0 -2.5" rotation="0 90 0"></a-box> -->
+
+	<a-camera wasd-controls="acceleration:40" id="camera">
 		
-			<a-entity  raycaster="objects:.clickable" cursor="fuse:false; fuseTimeout:2000;" geometry="primitive:sphere; radius-inner:0.01; radius-outer:0.03; radius:0.03" material="color:orange;" position="0 0 -2.5;"  animation__color=" property:material.color; from:##FFA500 ; to: #00FF00; dur: 100; startEvents:mouseenter;" animation__coloreset=" property:material.color; from:#00FF00 ; to: #FFA500; dur: 100; startEvents:mouseleave;" animation__fusing=" property:scale; from: 1 1 1; to: .5 .5 .5; dur: 500; startEvents:mouseenter;" animation__reset="property:scale; to: 1 1 1; startEvents:mouseleave;">		
+			<a-entity  raycaster="objects:.clickable" cursor="fuse:false; fuseTimeout:2000;" geometry="primitive:sphere;radius:0.03" material="color:orange;" position="0 0 -2.5;"  animation__color=" property:material.color; from:#FFA500 ; to: #00FF00; dur: 100; startEvents:mouseenter;" animation__coloreset=" property:material.color; from:#00FF00 ; to: #FFA500; dur: 100; startEvents:mouseleave;" animation__fusing=" property:scale; from: 1 1 1; to: .5 .5 .5; dur: 500; startEvents:mouseenter;" animation__reset="property:scale; to: 1 1 1; startEvents:mouseleave;">		
 			</a-entity>
 		
 	</a-camera>
+<a-entity gltf-model="#building" scale="2 2 2" position="-15 -0.5 17" rotation="0 90 0"></a-entity>
 
-</a-scene>
+</a-scene> 
+
+<script>
+
+
+</script>
 </body>
 </html>
 
